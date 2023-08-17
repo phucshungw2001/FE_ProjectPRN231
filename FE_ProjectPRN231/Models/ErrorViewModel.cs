e913408714b3829dd05aad42cef6723c64eec021
RoyTun <hungnphe150914@fpt.edu.vn>
2023-08-17 09:38:03 +0700

    update base

---
+++ FE_ProjectPRN231/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FE_ProjectPRN231.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
